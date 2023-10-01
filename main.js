cbc56352df1d53a2e48d592e3edd2514937c931f
tuankietcao <125777744+tuankietcao@users.noreply.github.com>
2023-10-01 14:50:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
